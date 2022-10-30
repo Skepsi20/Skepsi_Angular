dad56d372e20922f7e5253c39184b5f55cd429bb
omar.pineda <omarpinedareyes@gmail.com>
2022-10-29 20:40:09 -0500

    Add empty asyco1012 component

---
+++ src/app/componentes/rutinas/RutinasTerminadas/Synaisthimata/Antochi/Comprension/asyco1012/asyco1012.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asyco1012',
+  templateUrl: './asyco1012.component.html',
+  styleUrls: ['./asyco1012.component.css']
+})
+export class Asyco1012Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
